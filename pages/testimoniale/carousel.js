de480603c2e3ccd629a6e00628b2a8e5c0c0533f
Andrei Radu <andrei1.radu93@gmail.com>
2023-11-25 20:25:24 +0200

    mesaj

--- pages/testimoniale/carousel.js
+++ pages/testimoniale/carousel.js
@@ -19,6 +19,7 @@ const Testimonials = () => {
         description: "Colaborarea cu Consultify în cadrul programului Start-Up Nation a fost un pas important pentru afacerea mea. Echipa lor m-a sprijinit în fiecare etapă, de la verificarea eligibilității societății mele până la achizițiile necesare. Comunicarea clară și transparentă a fost un aspect de apreciat, iar ei au răspuns prompt la întrebările și nevoile mele.",
         descriptionTwo: "În urma aprobării proiectului, am beneficiat de ghidajul lor în identificarea și achiziționarea echipamentelor adecvate. Acum, aștept cu entuziasm începerea perioadei de monitorizare.",
         image: "/images/testimoniale/Recenzia 1.png",
+        static: "/images/testimoniale/static.png"
     },
     {
         id: 2,
@@ -27,6 +28,7 @@ const Testimonials = () => {
         description: "Cu ajutorul Consultify, am reușit să ne îndeplinim visul de a deschide o pensiune prin intermediul programului P.N.D.R. 6.4. Echipa lor ne-a ghidat și a oferit asistență în fiecare etapă a procesului, începând de la evaluarea eligibilității și redactarea proiectului, până la obținerea finanțării și implementarea sa. Comunicarea transparentă și suportul constant au fost de neprețuit.",
         descriptionTwo: "",
         image: "/images/testimoniale/Recenzia 2.png",
+        static: "/images/testimoniale/static.png"
     },
     {
         id: 3,
@@ -35,6 +37,7 @@ const Testimonials = () => {
         description: "Cu ajutorul echipei Consultify, am reușit să accesăm o finanțare de 40.000 de euro pentru carmangeria noastră. Echipa lor ne-a sprijinit de la verificarea eligibilității și scrierea proiectului, până la depunerea acestuia și construirea planului de achiziții. ",
         descriptionTwo: "De asemenea, ne-au ajutat să răspundem clarificărilor primite de la finanțator și să începem perioada de implementare. Colaborarea a fost remarcabilă, iar profesionalismul și implicarea lor ne-au asigurat șanse considerabile de reușită în acest proiect. Suntem recunoscători pentru ajutorul lor și recomandăm cu încredere serviciile Consultify.",
         image: "/images/testimoniale/Recenzia 3.png",
+        static: "/images/testimoniale/static.png"
     },
     {
         id: 4,
@@ -43,7 +46,7 @@ const Testimonials = () => {
         description: 'Cu sprijinul Consultify, am avut succes în obținerea finanțării necesare pentru achiziționarea de materii prime și echipamente prin intermediul programului "Microgranturi și Granturi în Domeniul Agroalimentar". Echipa lor ne-a fost alături în fiecare pas al procesului, de la evaluarea eligibilității și elaborarea proiectului, până la depunerea acestuia.  ',
         descriptionTwo: "Comunicarea fluentă și expertiza lor ne-au ajutat în demersurile noastre, iar profesionalismul de care au dat dovadă ne-a inspirat încredere. Recomand cu încredere Consultify!",
         image: "/images/testimoniale/Recenzia 4.gif",
-        static: "/images/testimoniale/Recenzia 4.png"
+        static: "/images/testimoniale/Recenzia 4.png",
     },
     {
         id: 5,
@@ -61,6 +64,7 @@ const Testimonials = () => {
         description: 'Cu sprijinul Consultify, am reușit să deschidem cu succes propria brutărie. Echipa lor ne-a oferit întreaga asistență în fiecare etapă a procesului, de la verificarea eligibilității și elaborarea proiectului, până la depunerea acestuia și gestionarea achizițiilor necesare.',
         descriptionTwo: "Comunicarea fluentă și abordarea lor profesionistă au fost cheia succesului proiectului nostru. Suntem foarte recunoscători pentru sprijinul lor.",
         image: "/images/testimoniale/Recenzia 6.png",
+        static: "/images/testimoniale/static.png"
     },
     {
         id: 7,
@@ -69,6 +73,7 @@ const Testimonials = () => {
         description: 'Cu sprijinul Consultify, am reușit să obțin finanțarea necesară pentru dezvoltarea afacerii. Colaborarea cu echipa lor a fost esențială în fiecare etapă a procesului. Încă de la început, mi-au verificat eligibilitatea  și am construit împreună un plan bugetar detaliat.',
         descriptionTwo: "Au fost ghidul meu de încredere în toți pașii necesari pentru depunerea proiectului și mi-au oferit suport constant și răspunsuri prompte la orice întrebare sau îngrijorare pe parcursul procesului. Îi recomand cu încredere!",
         image: "/images/testimoniale/Recenzia 7.png",
+        static: "/images/testimoniale/static.png"
     }
 ]
     return(
@@ -91,15 +96,13 @@ const Testimonials = () => {
                             <p className="text-sm text-[#fff] font-medium mt-2">{testimonial.descriptionTwo}</p>
                         </div>
                         <div className='relative w-[250px] md:w-[300px] h-[250px] md:h-[300px] mt-8 lg:mt-0 lg:mr-5 lg:min-w-max'>
-                            { testimonial.static &&
-                                <Image 
-                                    className="w-full h-full object-cover rounded-[24px] z-10"
-                                    src={testimonial.static}
-                                    alt={testimonial.title}
-                                    width={1080}
-                                    height={1080}
-                                />
-                            }
+                            <Image 
+                                className={"w-full h-full object-cover rounded-[24px] z-10" + (testimonial.static && ' hidden')}
+                                src={testimonial.static}
+                                alt={testimonial.title}
+                                width={1080}
+                                height={1080}
+                            />
                             <Image 
                                 className="absolute inset-0 w-full h-full object-cover rounded-[24px]"
                                 src={testimonial.image}
