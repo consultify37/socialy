7880e77db54ef6efaf9d96a91803ed8397d91ebd
Andrei Radu <andrei1.radu93@gmail.com>
2024-01-18 13:48:17 +0200

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({ slides, programe }: Props) {
     <>
       {/* pageSettings */}
       <Head>
-        <title>Socialy | Acasă</title>
+        <title>Socialy | Home</title>
       </Head>
       <HomeHeader slides={slides} />
       <TrustSRL />
