33e883c715137f5f93dc95ad0a1278d69858d7fd
Andrei Radu <andrei1.radu93@gmail.com>
2024-06-05 11:37:29 +0300

    .

--- utils/formatter.tsx
+++ utils/formatter.tsx
@@ -7,14 +7,26 @@ export const formatter = (node: any, index: number) => {
           <Image 
               key={node.attribs.src}
               src={node.attribs.src}
-              width={1024}
-              height={1024}
+              width={512}
+              height={512}
               alt='image'
+              unoptimized={true}
               className="w-full h-auto rounded-3xl mt-6 md:mt-8"
           />
       )
-  } else if (node.type === 'tag' && node.name === 'h2') {
-      
+  } if (node.type === 'tag' && node.name === 'p' && node.children[0].type === 'tag' && node.children[0].name === 'img') {
+			return (
+					<Image 
+							key={node.children[0].attribs.src}
+							src={node.children[0].attribs.src}
+							width={512}
+							height={512}
+							alt='image'
+							unoptimized={true}
+							className="w-full h-auto rounded-3xl mt-2 md:mt-8"
+					/>
+			)
+		} else if (node.type === 'tag' && node.name === 'h2') {     
       return (
           <h2
               key={node.children[0].data}
@@ -43,40 +55,26 @@ export const formatter = (node: any, index: number) => {
               {node.children[0].data}
           </h3>
       )
-  } else if (node.type === 'tag' && node.name === 'p' ) {
-		return node.children.map((node: any) => {
-			if ( node.data ) {
-				return (
-					<p
-						key={Math.random()}
-						className="text-[14px] md:text-[16px]"
-					>
-						{node.data}
-					</p>
-				)
-			} else if (node.type === 'tag' && node.name === 'img') {
-				return (
-						<Image 
-								key={node.attribs.src}
-								src={node.attribs.src}
-								width={1024}
-								height={1024}
-								alt='image'
-								className="w-full h-auto rounded-3xl mt-6 md:mt-8"
-						/>
-				)
-			} else if (node.type === 'tag' && node.name === 'a') {
-				return (
-					<Link
-						key={node.children[0].data}
-						href={node.attribs.href}
-						target="_blank"
-						className="text-[14px] md:text-[16px] text-blue-500 font-semibold"
-				>
-						{node.children[0].data}
-				</Link>
-				)
-			}
-		})
-  }
+  } else if (node.type === 'tag' && node.name === 'p'  ) {
+			return (
+				<p className="text-[14px] md:text-[16px]" key={Math.random()}>
+					{ node.children.length && node.children.map((node: any) => (
+							<span key={Math.random()}>
+								{ node.type === 'tag' && node.name === 'a' &&
+									<Link 
+										href={node.attribs.href} 
+										target="_blank" 
+										className="text-[14px] md:text-[16px] text-primary underline hover:scale-105 transition-all"
+									>
+										{ node.children[0].data }
+									</Link>
+								}
+								{ node.type === 'text' &&
+									node.data
+								}
+							</span>
+					))}
+				</p>
+			)
+	}
 }
