feaa7ff8c18d1a42bdf850a55741922c3cdc1f4b
Andrei Radu <andrei1.radu93@gmail.com>
2024-06-18 15:34:24 +0300

    .

--- components/Home/News/NewsItem.tsx
+++ components/Home/News/NewsItem.tsx
@@ -12,7 +12,7 @@ const NewsItem = ({ article, cardRef }: Props) => {
         <article className='w-[280px] bg-[#F5F5F5] rounded-[16px] snap-end shadow-lg flex flex-col h-full' ref={cardRef}>
             <Image src={article.mainImage.image} placeholder='blur' blurDataURL={article.mainImage.image} alt={article.title} width={300} height={300} className='w-full aspect-video h-48 object-cover rounded-[20px_20px_0_0]'/>
             <h3 className='text-secondary font-bold text-base p-2'>{article.title}</h3>
-            <p className='text-[#828282] text-[14px] pt-0 p-2'>{article.title}</p>
+            <p className='text-[#828282] text-[14px] pt-0 p-2'>{article.description}</p>
             <div className='flex justify-between pt-0 p-2 px-[10px] pb-3 mt-auto'>
                 <p className='text-[#828282] text-sm'>{article.formattedCreatedAt}</p>
                 <Link href={`/blog/${article.id}`} className='text-primary font-bold text-sm underline hover:scale-105 transition-all'>citește mai mult</Link>
